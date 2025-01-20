508b0881f8fd22ffaecc3e0e6aa31fdc5e3cebdf
Eric Pham <daggerstudiollc@gmail.comm>
2025-01-20 02:50:24 -0600

    first commit

--- src/components/ui/page.tsx
+++ src/components/ui/page.tsx
@@ -9,7 +9,7 @@ interface Props{
 export default function Page(props:Props){
     return(
         <>
-            <div id={props.Name} className={`${props.Name} h-auto mt-[1000px] sm:w-[80% w-[60%] mx-auto my-auto scroll-smooth`}>
+            <div id={props.Name} className={`${props.Name} h-auto mt-[1000px] sm:w-[80% w-[60%] mx-auto my-auto`}>
                 <h1 className="text-8xl py-8">{props.title}</h1>
                 {props.children}
             </div>
